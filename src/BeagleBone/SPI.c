2298da101f617c1eb21103769ffc03e7a4abdfbd
Samuel Sandelin <jrsandelin@gmail.com>
2024-04-16 12:00:02 -0700

    SPI communication function implementation

--- src/BeagleBone/SPI.c
+++ src/BeagleBone/SPI.c
@@ -4,7 +4,7 @@
 * Purpose: General purpose SPI communication implementation
 */
 
-//#include "SPI.h"
+#include "SPI.h"
 #include <stdint.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -15,13 +15,7 @@
 #include <linux/types.h>
 #include <linux/spi/spidev.h>
 
-#define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
-
-static void pabort(const char* s)
-{
-	perror(s);
-	abort();
-}
+#define pabort(s) perror(s); return 0;
 
 static const char* device = "/dev/spidev0.0";
 static uint8_t mode;
@@ -29,38 +23,51 @@ static uint8_t bits = 8;
 static uint32_t speed = 500000;
 static uint16_t delay;
 
-static void transfer(int fd)
-{
+uint8_t TransferSPI(SPI_t* spiControl, uint8_t* tx, uint8_t* rx, uint64_t len) {
 	int ret;
-	uint8_t tx[] = {
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-		0x40, 0x00, 0x00, 0x00, 0x00, 0x95,
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-		0xDE, 0xAD, 0xBE, 0xEF, 0xBA, 0xAD,
-		0xF0, 0x0D,
-	};
-	uint8_t rx[ARRAY_SIZE(tx)] = { 0, };
 	struct spi_ioc_transfer tr = {
 		.tx_buf = (unsigned long)tx,
 		.rx_buf = (unsigned long)rx,
-		.len = ARRAY_SIZE(tx),
+		.len = len,
 		.delay_usecs = delay,
 		.speed_hz = speed,
 		.bits_per_word = bits,
 	};
 
-	ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
+	ret = ioctl(spiControl->fd, SPI_IOC_MESSAGE(1), &tr);
 	if (ret < 1)
 		pabort("can't send spi message");
-
-	for (ret = 0; ret < ARRAY_SIZE(tx); ret++) {
-		if (!(ret % 6))
-			puts("");
-		printf("%.2X ", rx[ret]);
+	return 1;
+}
+uint8_t ReadSPI(SPI_t* spiControl, uint8_t* rx, uint64_t len) {
+	struct spi_ioc_transfer spi_message = {
+		.tx_buf = (unsigned long)0,
+		.rx_buf = (unsigned long)rx,
+		.len = len,
+		.delay_usecs = delay,
+		.speed_hz = speed,
+		.bits_per_word = bits,
 	}
-	puts("");
+
+	ret = ioctl(fd, SPI_IOC_MESSAGE(1), &spi_message);
+	if (ret < 1)
+		pabort("can't read from spi");
+	return 1;
+}
+uint8_t WriteSPI(SPI_t* spiControl, uint8_t* tx, uint64_t len) {
+	struct spi_ioc_transfer spi_message = {
+		.tx_buf = (unsigned long)tx,
+		.rx_buf = (unsigned long)0,
+		.len = len,
+		.delay_usecs = delay,
+		.speed_hz = speed,
+		.bits_per_word = bits,
+	}
+
+	ret = ioctl(fd, SPI_IOC_MESSAGE(1), &spi_message);
+	if (ret < 1)
+		pabort("can't write to spi");
+	return 1;
 }
 
 static void print_usage(const char* prog)
@@ -79,116 +86,44 @@ static void print_usage(const char* prog)
 	exit(1);
 }
 
-static void parse_opts(int argc, char* argv[])
+uint8_t StartSPI(SPI_t* spiControl)
 {
-	while (1) {
-		static const struct option lopts[] = {
-			{ "device",  0, 0, 'D' },
-			{ "speed",   1, 0, 's' },
-			{ "delay",   1, 0, 'd' },
-			{ "bpw",     1, 0, 'b' },
-			{ "loop",    0, 0, 'l' },
-			{ "cpha",    0, 0, 'H' },
-			{ "cpol",    0, 0, 'O' },
-			{ "lsb",     0, 0, 'L' },
-			{ "cs-high", 0, 0, 'C' },
-			{ "3wire",   0, 0, '3' },
-			{ "no-cs",   0, 0, 'N' },
-			{ "ready",   0, 0, 'R' },
-			{ NULL, 0, 0, 0 },
-		};
-		int c;
-
-		c = getopt_long(argc, argv, "D:s:d:b:lHOLC3NR", lopts, NULL);
-
-		if (c == -1)
-			break;
-
-		switch (c) {
-		case 'D':
-			device = optarg;
-			break;
-		case 's':
-			speed = atoi(optarg);
-			break;
-		case 'd':
-			delay = atoi(optarg);
-			break;
-		case 'b':
-			bits = atoi(optarg);
-			break;
-		case 'l':
-			mode |= SPI_LOOP;
-			break;
-		case 'H':
-			mode |= SPI_CPHA;
-			break;
-		case 'O':
-			mode |= SPI_CPOL;
-			break;
-		case 'L':
-			mode |= SPI_LSB_FIRST;
-			break;
-		case 'C':
-			mode |= SPI_CS_HIGH;
-			break;
-		case '3':
-			mode |= SPI_3WIRE;
-			break;
-		case 'N':
-			mode |= SPI_NO_CS;
-			break;
-		case 'R':
-			mode |= SPI_READY;
-			break;
-		default:
-			print_usage(argv[0]);
-			break;
-		}
-	}
-}
-
-int main(int argc, char* argv[])
-{
-	int ret = 0;
-	int fd;
-
-	parse_opts(argc, argv);
-
-	fd = open(device, O_RDWR);
-	if (fd < 0)
+	uint8_t ret = 0;
+	
+	spiControl->fd = open(device, O_RDWR);
+	if (spiControl->fd < 0)
 		pabort("can't open device");
 
 	/*
 	 * spi mode
 	 */
-	ret = ioctl(fd, SPI_IOC_WR_MODE, &mode);
+	ret = ioctl(spiControl->fd, SPI_IOC_WR_MODE, &mode);
 	if (ret == -1)
 		pabort("can't set spi mode");
 
-	ret = ioctl(fd, SPI_IOC_RD_MODE, &mode);
+	ret = ioctl(spiControl->fd, SPI_IOC_RD_MODE, &mode);
 	if (ret == -1)
 		pabort("can't get spi mode");
 
 	/*
 	 * bits per word
 	 */
-	ret = ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bits);
+	ret = ioctl(spiControl->fd, SPI_IOC_WR_BITS_PER_WORD, &bits);
 	if (ret == -1)
 		pabort("can't set bits per word");
 
-	ret = ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, &bits);
+	ret = ioctl(spiControl->fd, SPI_IOC_RD_BITS_PER_WORD, &bits);
 	if (ret == -1)
 		pabort("can't get bits per word");
 
 	/*
 	 * max speed hz
 	 */
-	ret = ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
+	ret = ioctl(spiControl->fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
 	if (ret == -1)
 		pabort("can't set max speed hz");
 
-	ret = ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &speed);
+	ret = ioctl(spiControl->fd, SPI_IOC_RD_MAX_SPEED_HZ, &speed);
 	if (ret == -1)
 		pabort("can't get max speed hz");
 
@@ -196,9 +131,12 @@ int main(int argc, char* argv[])
 	printf("bits per word: %d\n", bits);
 	printf("max speed: %d Hz (%d KHz)\n", speed, speed / 1000);
 
-	transfer(fd);
+	return 1;
+}
 
-	close(fd);
-
-	return ret;
+uint8_t StopSPI(SPI_t* spiControl) {
+	if (spiControl->fd < 0)
+		return 0;
+	close(spiControl->fd);
+	return 1;
 }
--- src/BeagleBone/headers/SPI.h
+++ src/BeagleBone/headers/SPI.h
@@ -5,8 +5,18 @@
 *
 * Author: Sam Sandelin (4/8/2024)
 * Purpose: General purpose SPI communication implementation
+*
+* Pulled and modified from the torvalds spidev_text.c (https://raw.githubusercontent.com/torvalds/linux/master/tools/spi/spidev_test.c)
 */
 
+typedef struct {
+    int fd;
+} SPI_t;
 
+uint8_t TransferSPI(SPI_t* spiControl, uint8_t* tx, uint8_t* rx, uint64_t len);
+uint8_t ReadSPI(SPI_t* spiControl, uint8_t* rx, uint64_t len);
+uint8_t WriteSPI(SPI_t* spiControl, uint8_t* tx, uint64_t len);
+uint8_t StartSPI(SPI_t* spiControl);
+uint8_t StopSPI(SPI_t* spiControl);
 
 #endif
